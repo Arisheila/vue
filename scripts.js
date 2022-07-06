02257ca73caa99f15f64a52ecc5f93c15d7df06f
Arisheila <sarinaitwe2013@gmail.com>
2022-07-06 10:21:48 +0300

    added vue project

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// const { createApp }= Vue
+
+// createApp({}).mount('#app')
+
+Vue.createApp({
+    data(){
+        return{
+        firstName:"shyla",
+        lastName:"Arinaitwe"
+        }
+    },
+}).mount('#app');
